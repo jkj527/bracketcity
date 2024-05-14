6573e55bffe8fceb3206db39d901709af2b5c681
Jake Johnson <jkjohnson1996@gmail.com>
2024-05-14 16:42:44 -0700

    react bracket library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Bracket from './components/bracket'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Bracket />
     </div>
   );
 }
---
+++ src/components/bracket.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Bracket, IRoundProps } from 'react-brackets';
+
+const rounds: IRoundProps[] = [
+  {
+    title: 'Round one',
+    seeds: [
+      {
+        id: 1,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 2,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+    ],
+  },
+  {
+    title: 'Round one',
+    seeds: [
+      {
+        id: 3,
+        date: new Date().toDateString(),
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+    ],
+  },
+];
+
+const bracket = () => {
+  return <Bracket rounds={rounds} />;
+};
+
+export default bracket;
