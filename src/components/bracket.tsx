3612e0bd8970c9cf05962ab893b3367043667d22
Jake Johnson <jkjohnson1996@gmail.com>
2024-05-15 21:56:44 -0700

    bracket extension

--- src/components/bracket.tsx
+++ src/components/bracket.tsx
@@ -1,28 +1,84 @@
 import React from "react";
 import { Bracket, IRoundProps } from 'react-brackets';
+import '../styles/bracket.css';
 
 const rounds: IRoundProps[] = [
   {
-    title: 'Round one',
+    title: 'Round One',
     seeds: [
       {
         id: 1,
-        date: new Date().toDateString(),
         teams: [{ name: 'Team A' }, { name: 'Team B' }],
       },
       {
         id: 2,
-        date: new Date().toDateString(),
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+      {
+        id: 3,
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 4,
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+      {
+        id: 5,
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 6,
+        teams: [{ name: 'Team C' }, { name: 'Team D' }],
+      },
+      {
+        id: 7,
+        teams: [{ name: 'Team A' }, { name: 'Team B' }],
+      },
+      {
+        id: 8,
         teams: [{ name: 'Team C' }, { name: 'Team D' }],
       },
     ],
   },
   {
-    title: 'Round one',
+    title: 'Quarter Finals',
     seeds: [
       {
-        id: 3,
-        date: new Date().toDateString(),
+        id: 9,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+      {
+        id: 10,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+      {
+        id: 11,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+      {
+        id: 12,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+    ],
+  },
+  {
+    title: 'Semifinals',
+    seeds: [
+      {
+        id: 13,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+      {
+        id: 14,
+        teams: [{ name: 'Team A' }, { name: 'Team C' }],
+      },
+    ],
+  },
+  {
+    title: 'Championship',
+    seeds: [
+      {
+        id: 13,
         teams: [{ name: 'Team A' }, { name: 'Team C' }],
       },
     ],
@@ -30,7 +86,11 @@ const rounds: IRoundProps[] = [
 ];
 
 const bracket = () => {
-  return <Bracket rounds={rounds} />;
+  return (
+    <div className="bracket">
+        <Bracket rounds={rounds} />;
+    </div>
+  )
 };
 
 export default bracket;
