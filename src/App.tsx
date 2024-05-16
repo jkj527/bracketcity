ec6faf8e2a44cc2fef58b5d9e97020677eda5cbe
Jake Johnson <jkjohnson1996@gmail.com>
2024-05-15 20:59:29 -0700

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Bracket from './components/bracket'
+import Bracket from './components/bracket';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Bracket />
     </div>
   );
