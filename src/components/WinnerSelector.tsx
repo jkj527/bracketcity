443e1ec82591a4d7f6805a812e2146fe9d52b8bf
Jake Johnson <jkjohnson1996@gmail.com>
2024-05-16 12:14:05 -0700

    winner selector handleclick outline

--- src/components/WinnerSelector.tsx
+++ src/components/WinnerSelector.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/WinnerSelector.css';
 import { survivors } from "../data";
 
 const WinnerSelector = () => {
+    const [contestants, setContestants] = useState(survivors);
+    const [options, setOptions] = useState([contestants[0], contestants[1]]);
+
+    const handleClick = () => {
+        // push winner to next round
+        // remove loser from contestants
+            // take slice of state, alter, set state
+        // reset options state to be next two survivors
+        
+    }
+
     return (
         <div className="winner">
-            <button>Survivor 1</button>
-            <button>Survivor 2</button>
+            <button onClick={handleClick} >{options[0]}</button>
+            <button onClick={handleClick} >{options[1]}</button>
         </div>
     )
 }
